1b68fdc0684bab6dfb315c38020cacf4c4f4a6bb
IT21174308 <it21174308@my.sliit.lk>
2024-04-12 16:53:18 +0530

    chnged DB

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 
 dotenv.config();
-const PORT = process.env.PORT || 8070
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 app.use(express.json());
